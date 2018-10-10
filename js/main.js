bb396428559dfc5f0c8024b8c9f30a848044e142
morphIsmail <ismail_2016@mail.ru>
2018-10-10 19:01:05 +0400

    Адаптивное меню

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(function(){
-
+   /* Адаптивное меню */
+   $('.icon span').click(function(){
+      if($('.menu').is(':hidden')) {
+         $('.menu').css('display','flex');
+         $('.icon span').html('&#10006;');
+      } 
+      else {
+         $('.menu').removeAttr('style');
+         $('.icon span').html('&#9776;');
+      }
+   });
+   /*  */
 });
